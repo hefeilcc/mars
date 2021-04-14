1f8e6f4da7243b0d122fda3450f9bf67feb3d4d8
hefeilcc <hefeilcc@163.com>
2021-04-14 20:51:23 +0800

    二级菜单

--- install.sh
+++ install.sh
@@ -4,7 +4,10 @@ set -x
 
 ps -ef | grep -w mars.py | grep -v grep | awk '{print $2}' | xargs kill >/dev/null 2>&1
 
-rm -rf /opt/mars
-cp -rf ../mars /opt
+#rm -rf /opt/mars
+#cp -rf ../mars /opt
+
+mkdir -p /opt/mars
+rsync -av --delete ./ /opt/mars/
 
 python -u /opt/mars/mars.py
